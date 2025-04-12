3a86e5c6bb146e43fa922126daefaa35f0afbc51
janani <designersaran8@gmail.com>
2025-04-12 11:25:53 +0530

    add banner

--- include/slider.php
+++ include/slider.php
@@ -18,15 +18,7 @@
                                             </a>
                                         </div>
                                     </div>
-                                    <div class="contact-info">
-                                        <div class="icon">
-                                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" enable-background="new 0 0 20 20" height="512" viewBox="0 0 20 20" width="512"><g><path d="m10.1 0c-5.8 0-10.1 2.3-10.1 5v.7c0 .7.6 1.2 1.3 1.3h2.6c.9 0 1.4-1.1 1.1-2-.5-.7 0-1.7.8-1.9 2.8-.6 5.6-.6 8.5 0 .9.2 1.3 1.1.8 1.9-.3.9.2 1.9 1.1 2h2.6c.7 0 1.3-.6 1.3-1.3v-.7c-.1-2.8-4.5-5-10-5z"/><g><path d="m15.3 7.5c-.2-.3-.5-.5-.9-.5h-1.4v-1.5c0-.3-.2-.5-.5-.5h-1c-.3 0-.5.2-.5.5v1.5h-2v-1.5c0-.3-.2-.5-.5-.5h-1c-.3 0-.5.2-.5.5v1.5h-1.4c-.4 0-.7.2-.9.5-1.5 2.6-3.7 5.9-3.7 11.5 0 .5.4 1 1 1h16c.6 0 1-.5 1-1 0-5.6-2.3-9.1-3.7-11.5zm-5.3 10.5c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z"/><circle cx="10" cy="14" r="2"/></g></g></svg>
-                                        </div>
-                                        <div class="left">
-                                            <h6>call us book a table & delivery</h6>
-                                            <h4><?php echo PHONE_NUMBER; ?></h4>
-                                        </div>
-                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -34,7 +26,7 @@
                 </div>
                 <div class="swiper-slide">
                     <div class="overlay">
-                        <img src="./assets/images/slider/img_slider_12.jpg" alt="">
+                        <img src="./assets/images/slider/banner-2.jpg" alt="">
                     </div>
                     <div class="container">
                         <div class="row">
@@ -50,15 +42,31 @@
                                             </a>
                                         </div>
                                     </div>
-                                    <div class="contact-info">
-                                        <div class="icon">
-                                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" enable-background="new 0 0 20 20" height="512" viewBox="0 0 20 20" width="512"><g><path d="m10.1 0c-5.8 0-10.1 2.3-10.1 5v.7c0 .7.6 1.2 1.3 1.3h2.6c.9 0 1.4-1.1 1.1-2-.5-.7 0-1.7.8-1.9 2.8-.6 5.6-.6 8.5 0 .9.2 1.3 1.1.8 1.9-.3.9.2 1.9 1.1 2h2.6c.7 0 1.3-.6 1.3-1.3v-.7c-.1-2.8-4.5-5-10-5z"/><g><path d="m15.3 7.5c-.2-.3-.5-.5-.9-.5h-1.4v-1.5c0-.3-.2-.5-.5-.5h-1c-.3 0-.5.2-.5.5v1.5h-2v-1.5c0-.3-.2-.5-.5-.5h-1c-.3 0-.5.2-.5.5v1.5h-1.4c-.4 0-.7.2-.9.5-1.5 2.6-3.7 5.9-3.7 11.5 0 .5.4 1 1 1h16c.6 0 1-.5 1-1 0-5.6-2.3-9.1-3.7-11.5zm-5.3 10.5c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z"/><circle cx="10" cy="14" r="2"/></g></g></svg>
-                                        </div>
-                                        <div class="left">
-                                            <h6>call us book a table & delivery</h6>
-                                            <h4>9350-0239</h4>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="overlay">
+                        <img src="./assets/images/slider/banner-3.jpg" alt="">
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="box-slider">
+                                    <div class="content-box">
+                                        <h2 class="title">The pleasure of <br> variety on your plate</h2>
+                                        <p class="sub-title">Craving some delicious Paris food? Maybe you’re in the mood
+                                            for a juicy steak? No matter what kind of meal you have in mind.</p>
+                                        <div class="wrap-btn">
+                                            <a href="menu_list.html" class="tf-button style2">
+                                                discovery menu
+                                            </a>
                                         </div>
                                     </div>
+                                    
                                 </div>
                             </div>
                         </div>
